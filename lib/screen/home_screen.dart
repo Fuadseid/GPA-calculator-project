7dcc2b9c67fc211e7348e280fadc89d8806522bc
Fuadseid <fuaddbus@gmail.com>
2024-08-29 22:16:24 +0300

    some update

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -39,10 +39,28 @@ class _HomeScreenState extends State<HomeScreen>
     _animationcontroller.forward();
   }
 
+  void showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Error"),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   void dispose() {
     _numberofCourseController.dispose();
-    _animationcontroller.dispose(); // Dispose of the controller
+    _animationcontroller.dispose();
     super.dispose();
   }
 
@@ -65,38 +83,64 @@ class _HomeScreenState extends State<HomeScreen>
             child: Center(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 200,
-                      alignment: Alignment.center,
-                      child: TextField(
-                        controller: _numberofCourseController,
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                          labelText: 'Number of course',
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 300,
+                        alignment: Alignment.center,
+                        
+                        child: TextField(
+                          controller: _numberofCourseController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            labelText: 'Number of course',
+                            border: OutlineInputBorder(),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 15),
-                    ElevatedButton(
-                      onPressed: _addFormScreen,
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Theme.of(context).colorScheme.primaryContainer,
+                      const SizedBox(height: 15),
+                      Container(
+                        height: 55,
+                        width: double.infinity,
+                        padding:const EdgeInsets.all(8),
+                      decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          const Color.fromARGB(255, 33, 243, 198),
+                          const Color.fromARGB(255, 7, 123, 98)
+                        ]),
+                    borderRadius: BorderRadius.all(Radius.circular(100)),
+                  ),
+                        child: TextButton(
+                          onPressed: () {
+                         
+                            if (_numberofCourseController.text.isEmpty ||
+                                double.tryParse(_numberofCourseController.text) ==
+                                    null) {
+                              showErrorDialog(
+                                  "Please fill The Number of The Course.");
+                            } else {
+                              _addFormScreen(); 
+                            }
+                          },
+                          child: Text(
+                            'Okay',
+                            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onPrimaryContainer,
+                                ),
+                          ),
                         ),
                       ),
-                      child: Text(
-                        'Okay',
-                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .onPrimaryContainer,
-                            ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -165,7 +209,7 @@ class _HomeScreenState extends State<HomeScreen>
       ),
     );
     Widget? content1;
-    if (gpa > 3.0) {
+    if (gpa >= 3.0) {
       content1 = Center(
         child: Container(
           width: 400,
@@ -212,7 +256,7 @@ class _HomeScreenState extends State<HomeScreen>
           ),
         ),
       );
-    } else if (gpa > 2.5 && gpa < 3.0) {
+    } else if (gpa >= 2.5 && gpa < 3.0) {
       content1 = Center(
         child: Container(
           width: 400,
